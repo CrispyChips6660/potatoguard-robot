f4c6a0a6862dc6a8c08339cefa4342671ad8cdce
cripsy_chips1234 <60465425+CrispyChips6660@users.noreply.github.com>
2020-08-16 12:28:05 +0800

    Change stuff

--- src/main/java/potatocult/potatoguard/PotatoGaurd.java
+++ src/main/java/potatocult/potatoguard/PotatoGaurd.java
@@ -2,12 +2,14 @@ package potatocult.potatoguard;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import potatocult.potatoguard.commands.PingCommand;
 
 import java.util.Scanner;
 
 public class PotatoGaurd {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception{
-        JDA jda = JDABuilder.createDefault(sc.next()).build();
+        JDA jda = JDABuilder.createDefault("not for you").build();
+        jda.addEventListener(new PingCommand());
     }
 }
