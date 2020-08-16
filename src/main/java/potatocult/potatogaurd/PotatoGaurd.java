9b7724d476283948f6b16d9ad04571784f1320b6
cripsy_chips1234 <60465425+CrispyChips6660@users.noreply.github.com>
2020-08-16 11:17:28 +0800

    Change stuff

--- src/main/java/potatocult/potatogaurd/PotatoGaurd.java
+++ src/main/java/potatocult/potatogaurd/PotatoGaurd.java
@@ -3,9 +3,11 @@ package potatocult.potatogaurd;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import java.util.Scanner;
+
 public class PotatoGaurd {
-    public static final String BOT_TOKEN = "NzQ0MzcxMzU0MDY5MTA2ODA5.XziP2A.ekYyTLhZUba1Nrjvq9Xh6ATkYJI";
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception{
-        JDA jda = JDABuilder.createDefault(BOT_TOKEN).build();
+        JDA jda = JDABuilder.createDefault(sc.next()).build();
     }
 }
